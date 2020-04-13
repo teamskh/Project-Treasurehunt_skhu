ef9e1b875c2411c3466042ba9c47ce56af1b84d7
yoon <pro_wh08@naver.com>
2020-04-13 18:15:09 +0900

    [yjh]씬추가

---
+++ 보물찾기/Assets/02.Scripts/csSceneTrans.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csSceneTrans : MonoBehaviour
+{
+    public void SceneTrans0_1()
+    {
+        Application.LoadLevel("02.memu");
+    }
+    public void SceneTrans1_1()
+    {
+        Application.LoadLevel("03.setting");
+    }
+    public void SceneTrans2_1()
+    {
+        Application.LoadLevel("04-0.select play");
+    }
+    public void SceneTrans2_2()
+    {
+        Application.LoadLevel("04-1.play");
+    }
+    public void SceneTrans2_3()
+    {
+        Application.LoadLevel("04-2.play");
+    }
+    public void SceneTrans3_1()
+    {
+        Application.LoadLevel("05.record");
+    }
+    public void SceneTrans4_1()
+    {
+        Application.LoadLevel("06.clear");
+    }
+}
