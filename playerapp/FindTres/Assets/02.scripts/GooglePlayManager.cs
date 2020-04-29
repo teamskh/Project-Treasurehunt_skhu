733960f8154a7cd54a46ffac1f3fa10ecc7d2986
yoon <pro_wh08@naver.com>
2020-04-29 21:48:07 +0900

    [yjh] google 로그인 기능
    구글 로그인만 가능 및 ar core 아직 추가 x

---
+++ playerapp/FindTres/Assets/02.scripts/GooglePlayManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Text UI 사용
+using UnityEngine.UI;
+// 구글 플레이 연동
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class GooglePlayManager : MonoBehaviour
+{
+    bool bWait = false;
+    public Text text;
+
+    void Awake()
+    {
+        PlayGamesPlatform.InitializeInstance(new PlayGamesClientConfiguration.Builder().Build());
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+
+        text.text = "no Login";
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+
+    public void OnLogin()
+    {
+        if (!Social.localUser.authenticated)
+        {
+            Social.localUser.Authenticate((bool bSuccess) =>
+            {
+                if (bSuccess)
+                {
+                    Debug.Log("Success : " + Social.localUser.userName);
+                    text.text = Social.localUser.userName;
+                }
+                else
+                {
+                    Debug.Log("Fall");
+                    text.text = "Fail";
+                }
+            });
+        }
+    }
+
+    public void OnLogOut()
+    {
+        ((PlayGamesPlatform)Social.Active).SignOut();
+        text.text = "Logout";
+    }
+}
