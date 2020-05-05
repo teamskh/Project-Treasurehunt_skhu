f2248242e3c082bb749a526ae7db8706f2f5e71e
yoon <pro_wh08@naver.com>
2020-05-05 23:35:01 +0900

    [yjh]scroll

---
+++ playerapp/FindTres/Assets/02.scripts/playerapp/quiz.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class quiz : MonoBehaviour
+{
+
+
+    InputField txt;
+    Text qtxt;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = GameObject.Find("InQu").GetComponent<InputField>(); //여러개는 find 값 수정하면 될듯??
+    
+       
+    }
+
+    public void inq()
+    {
+        quizLib.Instance.exam = txt.text;
+    }
+}
---
+++ playerapp/FindTres/Assets/02.scripts/playerapp/scenechange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechange : MonoBehaviour
+{
+    public void changeFirstScene()
+    {
+        SceneManager.LoadScene("sample1");
+    }
+
+    public void changeSecondScene()
+    {
+        SceneManager.LoadScene("sample2");
+    }
+}
