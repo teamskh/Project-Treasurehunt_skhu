8f2f1a9a621421432ffd37df524ea6ec2cb17f31
ParkYiJun <ydlwnsj25@gmail.com>
2020-06-08 22:49:11 +0900

    [PYJ]script for download Contents from github

---
+++ FindTreasure/Assets/02.scripts/Func/DownLoadFiles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class DownLoadFiles : MonoBehaviour
+{
+    string gitFileRoot = "https://github.com/teamskh/REPORT/tree/master/contents";
+    string downLoadPath = Application.persistentDataPath;
+    string FileName = "PlayerQuiz.dat";
+    WebClient wc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wc = new WebClient();
+        DownLoad();
+    }
+
+    private void DownLoad()
+    {
+        try
+        {
+            wc.DownloadFile(new Uri(gitFileRoot), downLoadPath + FileName);
+        }
+        catch(Exception e)
+        {
+            Debug.LogFormat("Error: {0}", e.ToString());
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
