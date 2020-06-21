3c8a4748156a4b1d30001b357fbf641dacdac8e3
prowh08 <pro_wh08@naver.com>
2020-06-21 18:55:43 +0900

    [yjh]중간저장

---
+++ FindTreasure/Assets/02.scripts/playerapp/realtimerank.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using LitJson;
+using BackEnd;
+using System.Collections.Generic;
+using System;
+using static BackEnd.BackendAsyncClass;
+
+public class realtimerank : MonoBehaviour
+{
+    private void test()
+    {
+        Backend.RTRank.GetRTRankByUuid("c104df80-aee4-11ea-8d65-076db9a598b3", 2); //상위 2명 랭킹정보 조회
+    }
+
+    public void 
+}
