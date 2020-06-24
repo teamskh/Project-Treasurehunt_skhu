b9134fd516b55f8676cc9ffd97f31f48092acadc
ParkYiJun <ydlwnsj25@gmail.com>
2020-06-24 18:35:35 +0900

    [pyj]

---
+++ FindTreasure/Assets/02.scripts/QuizForUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TTM.Classes;
+using UnityEngine;
+
+public class QuizForUI : MonoBehaviour
+{
+
+    public GameObject OX;
+    public GameObject sel;
+    public GameObject inpu;
+
+    private List<GameObject> list = new List<GameObject>();
+
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        Quiz quiz = gameman.Instance.FindQuiz();
+        int kind = quiz.Kind;
+        for(int i = 0; i < list.Count; i++)
+        {
+            if (i == kind)
+                list[i].SetActive(true);
+            else
+                list[i].SetActive(false);
+        }
+    }
+    private void Start()
+    {
+        list.Add(OX);
+        list.Add(sel);
+        list.Add(inpu);
+    }
+}
