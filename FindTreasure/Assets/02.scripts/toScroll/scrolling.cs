df038de8ca8245b19e978743c1594db06d6ac93e
ParkYiJun <ydlwnsj25@gmail.com>
2020-06-24 21:40:18 +0900

    Update scrolling.cs

--- FindTreasure/Assets/02.scripts/toScroll/scrolling.cs
+++ FindTreasure/Assets/02.scripts/toScroll/scrolling.cs
@@ -48,24 +48,29 @@ public class scrolling : MonoBehaviour
                 if (gameman.Instance != null)
                 {
                     Answer current = gameman.Instance.CheckAnswer();
-                    if (i == current.Kind)
-                    { //i 값이 스크롤내용 구분하는거
-                        chek[i].SetActive(true);
-                        //update에 넣은 이유는 문제들이 모두 setactive(false)되어있기때문에 못 찾는다
-                        if (i == 1)
-                        {
-                            Quiz Anslist = gameman.Instance.FindQuiz();
-                            Debug.Log(Anslist == null);
-                            if (current != null)
+
+                    if (current != null)
+                    {
+                        if (i == current.Kind)
+                        { //i 값이 스크롤내용 구분하는거
+                            chek[i].SetActive(true);
+                            //update에 넣은 이유는 문제들이 모두 setactive(false)되어있기때문에 못 찾는다
+                            if (i == 1)
                             {
-                                Ban = current.Banswer;
-                                for (int j = 0; j < 4; j++)
-                                    AnsTex[j].text = Anslist.List[j]; //AnsTex 글자
+                                Quiz Anslist = gameman.Instance.FindQuiz();
+                                Debug.Log(Anslist == null);
+                                if (Anslist != null)
+                                {
+                                    Ban = current.Banswer;
+                                    for (int j = 0; j < 4; j++)
+                                        AnsTex[j].text = Anslist.List[j]; //AnsTex 글자
+                                }
                             }
                         }
+                        else
+                            chek[i].SetActive(false);
                     }
-                    else
-                        chek[i].SetActive(false);
+                    
                 }
             }
             timer = 0;
