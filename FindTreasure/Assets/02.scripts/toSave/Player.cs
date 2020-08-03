fceabced160fb709ab417970d8418084d2b603c8
ParkYiJun <ydlwnsj25@gmail.com>
2020-08-03 21:52:03 +0900

    [pyj]Final Data Classes update ver.2

---
+++ FindTreasure/Assets/02.scripts/toSave/Player.cs
@@ -0,0 +1,72 @@
+﻿using BackEnd;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static bool isLogin = false;
+    public static bool LoginKind = false;
+
+    public static bool AdminLogin()
+    {
+        if (isLogin)
+        {
+            BackendReturnObject bro = new BackendReturnObject();
+            bro = Backend.BMember.CustomLogin("Admin", "toomuch");
+            if (bro.IsSuccess())
+            {
+                LoginKind = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static bool PlayerLogin()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration
+            .Builder()
+            .RequestServerAuthCode(false)
+            .RequestIdToken()
+            .RequestEmail()
+            .Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+        
+        while(PlayGamesPlatform.Instance.localUser.authenticated == false)
+        { 
+            Social.localUser.Authenticate(success =>
+            {   
+                if (success == false)
+                {
+                    Debug.Log("구글 로그인 실패");
+                    return;
+                }
+            });
+        }
+
+        BackendReturnObject bro = new BackendReturnObject();
+        bro = Backend.BMember.AuthorizeFederation(
+            PlayGamesPlatform.Instance.GetIdToken(), FederationType.Google);
+        if (bro.IsSuccess())
+        {
+            LoginKind = false;
+            return true;
+        }
+        else
+            return false;
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
