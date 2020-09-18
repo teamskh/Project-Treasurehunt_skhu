665afa124a572c45e735756e4359006ac91fc0a6
ParkYiJun <ydlwnsj25@gmail.com>
2020-09-19 01:54:54 +0900

    [PYJ]new Stack class for Backspace

---
+++ FindTreasure/Assets/02.scripts/toSave/BackSpace.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+public class BackSpace : Stack<string>
+{
+    #region Private Variable
+    #endregion
+
+    #region Public Variable
+    #endregion
+
+    #region Instance
+    private static BackSpace _Instance;
+    #endregion
+
+    #region Singleton
+    public static BackSpace Instance
+    {
+        get
+        {
+            if (_Instance == null) _Instance = new BackSpace();
+            return _Instance;
+        }
+    }
+    #endregion
+
+    public override string ToString()
+    {
+        string StackScenes = "";
+        foreach(var item in this)
+        {
+            StackScenes += item + "\n";
+        }
+        return StackScenes;
+    }
+}
