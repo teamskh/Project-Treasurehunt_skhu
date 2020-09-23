7160b070622c4d4fadd63dee7ee2496ae5d1caeb
Jaejeong98 <48522311+Jaejeong98@users.noreply.github.com>
2020-09-23 23:44:37 +0900

    [sjj]update

---
+++ FindTreasure/Assets/JaeJae/script/AdmistratorAppScripts/PanelScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PanelScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Panel;
+
+    private void Start()
+    {
+        Panel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        Forupdate();
+    }
+
+    void Forupdate()
+    {
+        if(GameObject.Find("Panel_pw")|| GameObject.Find("Panel_comp") || GameObject.Find("Panel_mode"))
+        {
+            Panel.SetActive(true);
+            if (Input.GetMouseButtonDown(0))
+            {
+                string obj_name=EventSystem.current.currentSelectedGameObject.name;
+                Debug.Log(obj_name);
+                if (obj_name == "Panel_T")
+                {
+                    Panel.SetActive(false);
+                }
+            }
+        }
+    }
+}
