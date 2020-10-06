e1185a4f4db4117cf12184ba3b365100a90ec79c
ParkYiJun <ydlwnsj25@gmail.com>
2020-10-07 00:55:43 +0900

    [PYJ]Quiz의 현재 인식이미지 불러오기

---
+++ FindTreasure/Assets/02.scripts/toSave/TextureExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using TTM.Classes;
+using UnityEngine;
+
+public static class TextureExtension {
+    public static Texture2D Load(this Texture2D dep , string compet, string quiz)
+    {
+        DataPath path = new DataPath("JPG/" + compet, quiz);
+        path.SetJPG();
+        byte[] bytetexture = File.ReadAllBytes(path.ToString());
+        if (bytetexture.Length > 0)
+        {
+            dep.name = quiz;
+            dep.LoadImage(bytetexture);
+            return dep;
+        }
+        return null;
+    }
+}
