d1c8eaa8e7c96d3042bdecff10037f7d785a1d80
prowh08 <pro_wh08@naver.com>
2020-10-10 22:34:23 +0900

    [yjh]record

--- FindTreasure/Assets/02.scripts/playerapp/createmyrank.cs
+++ FindTreasure/Assets/02.scripts/playerapp/createmyrank.cs
@@ -12,82 +12,31 @@ public class createmyrank : MonoBehaviour
     private List<GameObject> buttons = new List<GameObject>();
     public List<string> ConName;
 
-    public void Start()
-    {
-
-
-        //PlayerPrefs.DeleteAll();
-
-            /*GameObject go = Instantiate(Rprefab, Rprefab.transform.position, Quaternion.identity) as GameObject;
-            go.transform.GetChild(0).transform.GetComponent<Text>().text = PlayerPrefs.GetString("ConName");
-            go.transform.GetChild(1).transform.GetComponent<Text>().text = PlayerPrefs.GetString("Times");
-            go.transform.GetChild(2).transform.GetComponent<Text>().text = PlayerPrefs.GetString("Score");
-            Adapter.transform.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, wordNumber * 112);*/
-        /*if (gameman.Instance.loadRankChek == true)
-        {
-            nextNumber++;
-
-            if (PlayerPrefs.HasKey("nextNumber"))
-                nextNumber = PlayerPrefs.GetInt("nextNumber");
-
-            GameObject go = Instantiate(prefab, prefab.transform.position, Quaternion.identity) as GameObject;
-            
-            string wordString = conname;
-            Debug.Log("****");
-            PlayerPrefs.SetString("words" + wordNumber, wordString);
-
-            Debug.Log("*5***");
-            Debug.Log(go.activeInHierarchy);
-            Debug.Log(go.activeSelf);
-
-            go.transform.GetChild(0).transform.GetComponent<Text>().text = conname; //대회 이름
-            go.transform.GetChild(1).transform.GetComponent<Text>().text = rankTime; //시간
-            go.transform.GetChild(2).transform.GetComponent<Text>().text = rankScore.ToString(); //점수
-
-            Debug.Log("**8*");
-            go.transform.SetParent(Content.transform);
-            Debug.Log("**9*");
-            PlayerPrefs.SetInt("nextNumber", nextNumber);
-            wordNumber++;
-            PlayerPrefs.SetInt("wordNumber" + wordNumber, wordNumber);
-
-            Content.transform.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, wordNumber * 112);
-            gameman.Instance.loadRankChek = false;
-            
-        }
-
-        //PlayerPrefs.DeleteAll();//deleted all playerprefs
-
-        wordNumber = PlayerPrefs.GetInt("nextNumber");
-        Debug.Log("**"+wordNumber);
-        Content.transform.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, wordNumber * 112);
-
-        for (int i = 0; i < wordNumber; i++)
-        {
-            GameObject go = Instantiate(prefab, prefab.transform.position, Quaternion.identity) as GameObject;
-            go.transform.GetChild(0).transform.GetComponent<Text>().text = conname; //대회 이름
-            go.transform.GetChild(1).transform.GetComponent<Text>().text = rankTime; //시간
-            go.transform.GetChild(2).transform.GetComponent<Text>().text = rankScore.ToString(); //점수
-
-            go.transform.SetParent(Content.transform);
-        }
-        */
-    }
-
-    private void OnEnable()
+    private void Awake()
     {
         curlist = PlayerContents.Instance.CompetitionList();
         foreach (string title in curlist)
         {
             GameObject b = Instantiate(CompetR, transform);
             b.GetComponent<RectTransform>().anchoredPosition.Set(0, (buttons.Count + 1) * 60);
-            foreach (Text t in b.transform.GetComponentsInChildren<Text>(true))
+            foreach (Text t in b.GetComponentsInChildren<Text>())
             {
-                if (t.gameObject.layer == 8)
+                if (t.gameObject.layer == 0) //대회이름
+                {
+                    t.text = "0";
+                    ConName.Add(title);
+                }
+                else if(t.gameObject.layer == 9) //시간 
                 {
                     t.text = title;
                     ConName.Add(title);
                 }
+                else if (t.gameObject.layer == 5) //점수
+                {
+                    t.text = title;
+                    ConName.Add(title);
+                }
+                
             }
         }
     }
